3def4a97956301303515753e60eed2416ffb8a01
gcc_bug <by.gdc@outlook.com>
2024-01-03 14:38:28 +0800

    finish row-op

--- include/alg/row-op.hpp
+++ include/alg/row-op.hpp
@@ -12,12 +12,12 @@
 #include "seprate.hpp"
 
 namespace lq {
-    std::vector<std::string> topDown1(Sttree* root,const std::set<int> leaves){
+    std::vector<std::pair<int,int>> topDown1(Sttree* root,const std::set<int> leaves){
         if (root == nullptr) return {};
-        std::vector<std::string> gates, temp;
+        std::vector<std::pair<int,int>> gates, temp;
 
         for (auto child : root->children) {
-            gates.push_back("cx q["+std::to_string(root->data)+"] "+" q["+std::to_string(child->data)+"];\n");
+            gates.push_back(std::make_pair(root->data,child->data));
             if(leaves.find(child->data) == leaves.end()){
                 temp = topDown1(child, leaves);
                 gates.insert(gates.end(),temp.begin(),temp.end());
@@ -27,9 +27,9 @@ namespace lq {
         return gates;
     }
 
-    std::vector<std::string> bootomUp1(Sttree* root,const std::set<int> leaves){
+    std::vector<std::pair<int,int>> bootomUp1(Sttree* root,const std::set<int> leaves){
         if (root == nullptr) return {};
-        std::vector<std::string> gates, temp;
+        std::vector<std::pair<int,int>> gates, temp;
 
         for (auto child : root->children) {
             if(leaves.find(child->data) == leaves.end()){
@@ -42,13 +42,13 @@ namespace lq {
     }
 
 
-    std::vector<std::string> bootomUp2(Sttree* root,const std::set<int> leaves){
+    std::vector<std::pair<int,int>> bootomUp2(Sttree* root,const std::set<int> leaves){
         if (root == nullptr) return {};
-        std::vector<std::string> gates, temp;
+        std::vector<std::pair<int,int>> gates, temp;
 
         for (auto child : root->children) {
             if(leaves.find(child->data) == leaves.end()){
-                gates.push_back("cx q["+std::to_string(root->data)+"] "+" q["+std::to_string(child->data)+"];\n");
+                gates.push_back(std::make_pair(root->data,child->data));
                 temp = bootomUp2(child, leaves);
                 gates.insert(gates.end(),temp.begin(),temp.end());
             }
@@ -57,9 +57,9 @@ namespace lq {
         return gates;
     }
 
-    std::vector<std::string> topDown2(Sttree* root,const std::set<int> leaves){
+    std::vector<std::pair<int,int>> topDown2(Sttree* root,const std::set<int> leaves){
         if (root == nullptr) return {};
-        std::vector<std::string> gates, temp;
+        std::vector<std::pair<int,int>> gates, temp;
 
         for (auto child : root->children) {
             if(leaves.find(child->data) == leaves.end()){
@@ -69,59 +69,72 @@ namespace lq {
         }
         
         return gates;
+    }
+
+    xt::xarray<int> mod2add(xt::xarray<int>& A, int row_i, int row_j){
+        row_i --;
+        row_j --;
+        if (row_i < A.shape()[0] && row_j < A.shape()[0]) {
+            xt::view(A, row_i, xt::all()) = xt::eval(xt::view(A, row_i, xt::all()) + xt::view(A, row_j, xt::all())) % 2;
+            return A;
+        }
+        else{
+            throw std::invalid_argument("errur");
+        }
     }
 
     void rowOp(xt::xarray<int>& A, std::set<int>& terminals, Sttree* root, size_t alg) {
         auto Ts = seprate(root,root->data,terminals,alg);
 
-        std::vector<std::string> temp;
+        std::vector<std::pair<int,int>> paths;
         for(int i = Ts.size()-1; i>=0; --i){
             if(alg!=1){
                 std::cout << "Bootom Up 1" << std::endl;
-                temp = bootomUp1(Ts.at(i).root,Ts.at(i).leaves);
-                std::reverse(temp.begin(),temp.end());
-                for(auto item: temp){
-                    std::cout << item;
-                }
-                if(alg != 4){
-
+                paths = bootomUp1(Ts.at(i).root,Ts.at(i).leaves);
+                std::reverse(paths.begin(),paths.end());
+                for(auto qubits: paths){
+                    std::cout << qubits.first << " " << qubits.second << "\n";
+                    if(alg != 4){
+                        A = mod2add(A,qubits.second,qubits.first);
+                    }
                 }   
             }
             // Top-Down-1
             std::cout << "Top Down 1" << std::endl;
-            temp = topDown1(Ts.at(i).root,Ts.at(i).leaves);
-            for(auto item: temp){
-                std::cout << item;
-            }
-            if(alg != 4){
-
+            paths = topDown1(Ts.at(i).root,Ts.at(i).leaves);
+            for(auto qubits: paths){
+                std::cout << qubits.first << " " << qubits.second << "\n";
+                if(alg != 4){
+                    A = mod2add(A,qubits.second,qubits.first);
+                }
             }
             // Bootom-Up-2
             std::cout << "Bootom up 2" << std::endl;
-            temp = bootomUp2(Ts.at(i).root,Ts.at(i).leaves);
-            std::reverse(temp.begin(),temp.end());
-            for(auto item: temp){
-                std::cout << item;
-            }
-            if(alg != 4){
-
+            paths = bootomUp2(Ts.at(i).root,Ts.at(i).leaves);
+            std::reverse(paths.begin(),paths.end());
+            for(auto qubits: paths){
+                std::cout << qubits.first << " " << qubits.second << "\n";
+                if(alg != 4){
+                    A = mod2add(A,qubits.second,qubits.first);
+                }
             }
             if(alg!=1){
                 // Top-Down-2
                 std::cout << "Top Down 2" << std::endl;
-                temp = topDown2(Ts.at(i).root,Ts.at(i).leaves);
-                for(auto item: temp){
-                    std::cout << item;
-                }
-                if(alg != 4){
-
+                paths = topDown2(Ts.at(i).root,Ts.at(i).leaves);
+                for(auto qubits: paths){
+                    std::cout << qubits.first << " " << qubits.second << "\n";
+                    if(alg != 4){
+                        A = mod2add(A,qubits.second,qubits.first);
+                    }
                 }   
             }
             if(alg==4){
-
+                for(auto leaf: terminals){
+                    A = mod2add(A,root->data,leaf);
+                }
             }
         }
-
     }
 }
 
--- test/test_ro.cpp
+++ test/test_ro.cpp
@@ -7,26 +7,29 @@
 using namespace lq;
 int main(){
     Sttree* node1 = new Sttree(1);
+    Sttree* node2 = new Sttree(2);
+    Sttree* node3 = new Sttree(3);
     Sttree* node4 = new Sttree(4);
     Sttree* node5 = new Sttree(5);
     Sttree* node6 = new Sttree(6);
-    Sttree* node7 = new Sttree(7);
-    Sttree* node8 = new Sttree(8);
-    Sttree* node11 = new Sttree(11);
 
-    insertChild(node1,node4);
+    insertChild(node1,node2);
+    insertChild(node2,node3);
+    insertChild(node3,node4);
     insertChild(node4,node5);
-    insertChild(node5,node6);
-    insertChild(node4,node7);
-    insertChild(node7,node8);
-    insertChild(node8,node11);
 
     traverse(node1);
     exportTreeToDot(node1,"tree");
 
-    std::set<int> terminals = {6,7,11};
+    std::set<int> terminals = {3,4,5};
 
-    xt::xarray<int> A {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    xt::xarray<int> A = {{1, 1, 0, 1, 1, 0},
+                         {0, 0, 1, 1, 0, 1},
+                         {1, 0, 1, 0, 1, 0},
+                         {1, 1, 0, 1, 0, 0},
+                         {1, 1, 1, 1, 0, 0},
+                         {0, 1, 0, 1, 0, 1}};
+
     rowOp(A,terminals,node1,3);
     std::cout << A ;
     return 0;
