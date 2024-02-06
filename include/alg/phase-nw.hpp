353bc1fcaa6af9b4763b5517015ab504f762ce6c
gcc-bug <by.gdc@outlook.com>
2024-02-07 02:52:32 +0800

    add class

--- include/alg/phase-nw.hpp
+++ include/alg/phase-nw.hpp
@@ -8,7 +8,7 @@
 #ifndef ALG_PN
 #define ALG_PN
 #include "typedef.hpp"
-
+#include <stack>
 namespace lq{
     enum class Coeffs {
         COEFF_0 = 0,
@@ -25,70 +25,113 @@ namespace lq{
     }
     class ParityTerm{
         private:
-        std::vector<bool> terms;
-        bool flip;
-        Coeffs c;
+            xt::xarray<bool> terms;
+            std::vector<bool> flips;
+            std::vector<Coeffs> coeffs;
+            std::set<size_t> workTerms;
+            LabelIndexBiMap biMap;
 
+            bool inline isValid() const {
+                auto dims = terms.shape();
+                if (dims.size() != 2) return false; // Ensure there are exactly two dimensions
+
+                size_t cols = dims[1]; 
+                size_t rows = dims[0];
+
+                return cols == flips.size() &&
+                    cols == coeffs.size() &&
+                    cols == workTerms.size() &&
+                    rows == biMap.getSize();
+            }
         public:
-        ParityTerm(const std::vector<bool>& terms_,bool flip_,int c_): terms(terms_),flip(flip_){
-            if(isValidCoeffValue(c_)) this->c = static_cast<Coeffs>(c_);
-            else throw std::invalid_argument("Erruer");
-        }
-
-        bool at(size_t pos) const{
-            return this->terms[pos];
-        }
-        
-    };
-    
-    bool isUniform(const std::vector<ParityTerm>& terms, size_t pos) {
-        if (terms.empty()) return true;
-        bool temp = terms.front().at(pos);
-        for (size_t j = 1; j < terms.size(); ++j) {
-            if (terms[j].at(pos) != temp) return false;
-        }
-        return true;
-    }
-
-    int countUniforms(const std::vector<ParityTerm>& B, const std::set<size_t>& poss) {
-        if (B.empty()) return 0;
-        int res = 0;
-        for (auto& pos : poss) {
-            if (isUniform(B, pos)) ++res;
-        }
-        return res;
-    }
-
-    std::pair<std::vector<ParityTerm>, std::vector<ParityTerm>> splitByValue(const std::vector<ParityTerm>& terms, size_t pos) {
-        std::vector<ParityTerm> P0, P1;
-        for (const auto& b : terms) {
-            (b.at(pos) ? P1 : P0).push_back(b);
-        }
-        return {P0, P1};
-    }
-
-    std::pair<size_t, std::pair<std::vector<ParityTerm>,std::vector<ParityTerm>>> findOptimal(const std::vector<ParityTerm>& terms, const std::set<size_t>& poss) {
-        size_t max_pos = *poss.begin(); // Safe default if I is guaranteed to be non-empty.
-        int max = -1; // Start with a value that any result will surpass.
-        std::vector<ParityTerm> P0, P1;
-
-        for (auto pos : poss) {
-            std::cout << pos << std::endl;
-            auto [P0_, P1_] = splitByValue(terms, pos);
-            std::cout << P0_.size() <<" " << P1_.size() << std::endl;
-            int temp = countUniforms(P0_, poss) + countUniforms(P1_, poss);
-            std::cout << "cout: " << temp << std::endl;
-            if (max < temp) {
-                max = temp;
-                max_pos = pos;
-                P0 = P0_;
-                P1 = P1_;
+        ParityTerm(const xt::xarray<bool>& terms_, std::vector<bool> flip_, std::vector<int> cfs, const LabelIndexBiMap& biMap_): terms(terms_),flips(flip_), biMap(biMap_){
+            for(const auto& c: cfs){
+                if(isValidCoeffValue(c)) this->coeffs.push_back(static_cast<Coeffs>(c));
+                else throw std::invalid_argument("Erruer");
+            }
+            for (int i = 0; i <= flip_.size(); ++i) {
+                this->workTerms.insert(i);
             }
         }
-        return {max_pos,{P0,P1}};
-    }
-    void phaseNW(std::vector<ParityTerm> B, Graph g){
+        ParityTerm(const xt::xarray<bool>& terms_, std::vector<bool> flip_, std::vector<Coeffs> cfs, const LabelIndexBiMap& biMap_): terms(terms_),flips(flip_), biMap(biMap_),coeffs(cfs){
+            for (int i = 0; i <= flip_.size(); ++i) {
+                this->workTerms.insert(i);
+            }
+        }
 
+        ParityTerm(const xt::xarray<bool>& terms_, const LabelIndexBiMap& biMap_) : terms(terms_), biMap(biMap_) {}
+
+        bool at(size_t i,size_t pos) const{
+            return this->terms(pos,i);
+        }
+
+        bool isUniform(const std::set<size_t>& compareTerms, const size_t pos) const {
+            if (compareTerms.empty()) return true; // Handle empty set case
+
+            auto firstTermIt = compareTerms.begin();
+            bool temp = this->terms(pos,*firstTermIt);
+
+            // Iterate over the set starting from the second element
+            for (auto it = std::next(firstTermIt); it != compareTerms.end(); ++it) {
+                if (this->terms(pos,*it) != temp) return false;
+            }
+            return true;
+        }
+
+        int countUniforms(const std::set<size_t>& compareTerms, const std::set<size_t>& poss) const{
+            if (compareTerms.empty()) return 0;
+            int res = 0;
+            for (auto& pos : poss) {
+                if (isUniform(compareTerms, pos)) ++res;
+            }
+            return res;
+        }
+
+        std::pair<std::set<size_t>, std::set<size_t>> splitByValue(const std::set<size_t>& compareTerms, size_t pos) const{
+            std::set<size_t> P0, P1;
+            for (const auto& c : compareTerms) {
+                (this->terms(pos,c) ? P1 : P0).insert(c);
+            }
+            // std::cout << "P0:" << std::endl;
+            // for(auto p: P0){
+            //     std::cout << p << " ";
+            // }
+            // std::cout << std::endl;
+            // std::cout << "P1:" << std::endl;
+            // for(auto p: P1){
+            //     std::cout << p << " ";
+            // }
+            // std::cout << std::endl;
+            return {P0, P1};
+        }
+
+        std::pair<size_t, std::pair<std::set<size_t>,std::set<size_t>>> findOptimal(const std::set<size_t>& compareTerms, const std::set<size_t>& poss ) const{
+            size_t max_pos = *poss.begin(); // Safe default if I is guaranteed to be non-empty.
+            int max = -1; // Start with a value that any result will surpass.
+            std::set<size_t> P0, P1;
+
+            for (auto pos : poss) {
+                // std::cout << pos << std::endl;
+                auto [P0_, P1_] = splitByValue(compareTerms, pos);
+                // std::cout << P0_.size() <<" " << P1_.size() << std::endl;
+                int temp = countUniforms(P0_, poss) + countUniforms(P1_, poss);
+                // std::cout << "cout: " << temp << std::endl;
+                if (max < temp) {
+                    max = temp;
+                    max_pos = pos;
+                    P0 = P0_;
+                    P1 = P1_;
+                }
+            }
+            return {max_pos,{P0,P1}};
+        }
+    };
+
+    
+    void phaseNW(std::vector<ParityTerm> B, Graph g){
+        using StackType = std::tuple<std::vector<ParityTerm>, std::set<label>, label>;
+        std::stack<StackType> myStack;
+        // label 
     }
 }
 
--- test/test_pw.cpp
+++ test/test_pw.cpp
@@ -10,15 +10,18 @@ int main(){
     g.addEdge(4,5);
     g.addEdge(5,6);
 
-    ParityTerm p1 = ParityTerm({1,0,0,1,1,0},1,1);
-    ParityTerm p2 = ParityTerm({0,1,1,0,1,1},0,2);
-    ParityTerm p3 = ParityTerm({0,0,0,1,1,1},1,4);
-    ParityTerm p4 = ParityTerm({1,1,0,0,0,1},1,4);
-    ParityTerm p5 = ParityTerm({1,1,1,0,0,0},1,6);
-    ParityTerm p6 = ParityTerm({1,1,0,1,0,1},1,7);
-    ParityTerm p7 = ParityTerm({0,1,0,1,1,0},0,1);
+    xt::xarray<bool> B ={{1, 0, 0, 1, 1, 1, 0},
+                        {0, 1, 0, 1, 1, 1, 1},
+                        {0, 1, 0, 0, 1, 0, 0},
+                        {1, 0, 1, 0, 0, 1, 1},
+                        {1, 1, 1, 0, 0, 0, 1},
+                        {0, 1, 1, 1, 0, 1, 0}};
+    std::vector<bool> flips = {1, 0, 1, 1, 1, 1, 0};
+    std::vector<int> cs = {1, 2, 4, 4, 6, 7, 1};
+    LabelIndexBiMap biMap(std::set<label>{1, 2, 3, 4, 5, 6});
 
-    auto j = findOptimal({p1,p2,p3,p4,p5,p6,p7},{0,1,2,3,4,5});
-    std::cout << "j: " << j.first <<std::endl;
+    ParityTerm pt = ParityTerm(B,flips,cs,biMap);
+    auto j = pt.findOptimal({0,1,2,3,4,5,6},{0,1,2,3,4,5}).first;
+    std::cout << "j: " << j <<std::endl;
     return 0;
 }
